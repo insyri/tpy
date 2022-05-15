ef55519f11471cd44f3a113dfd764e60befaeb3f
insyri <60794909+insyri@users.noreply.github.com>
2022-05-15 00:32:14 -0400

    modts

---
+++ mod.ts
@@ -0,0 +1,6 @@
+import * as APITypes from "./src/api.d.ts";
+import * as Minitypes from "./src/types/minitypes.d.ts";
+import Tpy from "./src/tpy.ts";
+
+export default Tpy;
+export { APITypes, Minitypes };
